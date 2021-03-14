aa53b4e49f8a2b4273b30c1744dfbd6555a7dd87
taras-gr <tar.grytsenko@gmail.com>
2021-03-14 21:31:07 +0200

    Lab1 is done

---
+++ LabsLibrary/Labs/Lab1/Edge.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabsLibrary.Labs.Lab1
+{
+    class Edge : IComparable<Edge>
+    {
+        public int v1, v2;
+
+        public int weight;
+
+        public Edge(int v1, int v2, int weight)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.weight = weight;
+        }
+
+        public int CompareTo(Edge other)
+        {
+            if (this.weight > other.weight)
+                return 1;
+            if (this.weight < other.weight)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    static class PrimsAlgorithmProcessor
+    {
+        public static List<Edge> GetMSTByPrimsAlgorithm(List<Edge> edges)
+        {
+            var mst = new List<Edge>();
+            List<Edge> notUsedEdges = new List<Edge>(edges);
+            List<int> usedVertices = new List<int>();
+            List<int> notUsedVertices = new List<int>();
+
+            var verticesNumber = edges.Select(s => s.v1).Union(edges.Select(s => s.v2)).Count();
+
+            for (int i = 0; i < verticesNumber; i++)
+                notUsedVertices.Add(i);
+
+            // Визначення випадкової пешої вершини, з якої починати роботу
+            Random rand = new Random();
+            usedVertices.Add(rand.Next(0, verticesNumber));
+            Console.WriteLine($"\nThe first vertice is {usedVertices[0]}");
+            notUsedVertices.RemoveAt(usedVertices[0]);
+
+            while (notUsedVertices.Count > 0)
+            {
+                // Пошук мінімального суміжного ребра до вже задіяних вершин
+                var minEdge = notUsedEdges.Where(s => usedVertices.Contains(s.v1) || usedVertices.Contains(s.v2)).Min();
+
+                // Додавання вершини в список "задіяних"
+                if (usedVertices.Contains(minEdge.v1))
+                {
+                    usedVertices.Add(minEdge.v2);
+                    notUsedVertices.Remove(minEdge.v2);
+                }
+                    
+                else
+                {
+                    usedVertices.Add(minEdge.v1);
+                    notUsedVertices.Remove(minEdge.v1);
+                }
+
+                Console.WriteLine($"Adding edge (v1 = {minEdge.v1}, v2 = {minEdge.v2}, weight = {minEdge.weight}) to MST");
+
+                //Додавання ребра в дерево
+                mst.Add(minEdge);
+                notUsedEdges.Remove(minEdge);
+            }
+
+            return mst;
+        }
+    }
+}
---
+++ LabsLibrary/Labs/Lab1/Lab1.cs
@@ -0,0 +1,67 @@
+﻿using Common.Core.Interfaces;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LabsLibrary.Labs.Lab1
+{
+    class Lab1 : IRunnable
+    {
+        public void Run()
+        {
+            var incidenceMatrix = GetMatrixFromFile();
+            Console.WriteLine("\nThe incidence matrix is: ");
+            for (int i = 0; i < incidenceMatrix.Length; i++)
+            {
+                for (int j = 0; j < incidenceMatrix[i].Length; j++)
+                {
+                    Console.Write(incidenceMatrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            var edges = GetListOfEdgesFromMatrix(incidenceMatrix);
+
+            var mstByPrimsAlgorithm = PrimsAlgorithmProcessor.GetMSTByPrimsAlgorithm(edges);
+
+            
+            Console.WriteLine("\nThe minimum spanning tree by prim's algorithm is: ");
+
+            for (int i = 0; i < mstByPrimsAlgorithm.Count; i++)
+            {
+                Console.WriteLine($"The {i}st edge is: (v1 = {mstByPrimsAlgorithm[i].v1}, v2 = {mstByPrimsAlgorithm[i].v2}, weight = {mstByPrimsAlgorithm[i].weight})");
+            }
+        }
+
+        private List<Edge> GetListOfEdgesFromMatrix(int[][] incidenceMatrix)
+        {
+            var edges = new List<Edge>();
+
+            for (int i = 0; i < incidenceMatrix.Length; i++)
+            {
+                for (int j = 1 + i; j < incidenceMatrix[i].Length; j++)
+                {
+                    if (incidenceMatrix[i][j] != 0)
+                        edges.Add(new Edge(i, j, incidenceMatrix[i][j]));
+                }
+            }
+
+            return edges;
+        }
+
+        private int[][] GetMatrixFromFile()
+        {
+            var jsonTestDataFileName = "lab1Data.json";
+            var absolutePathForDocs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
+                + jsonTestDataFileName);
+
+            var json = File.ReadAllText(absolutePathForDocs);
+            var jObject = JObject.Parse(json);
+            var values = jObject["incidenceMatrix"]?.ToObject<int[][]>();
+
+            return values != null ? values : throw new ArgumentNullException();
+        }
+    }
+}
