77827e470a737e9f403e37288b4ec366d27747a9
taras-gr <tar.grytsenko@gmail.com>
2021-04-03 20:17:45 +0300

    Lab4 is done

---
+++ LabsLibrary/Labs/Lab4/FordFulkersonProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LabsLibrary.Labs.Lab4
+{
+    public class FordFulkersonProcessor
+    {
+        public int CalculateMaxFlow(int[][] cap, int vertexFrom, int vertexTo)
+        {
+            var maxFlowValue = 0;
+
+            while (true)
+            {
+                var foundFlow = FindPath(cap, new bool[cap.Length], vertexFrom, vertexTo, int.MaxValue);
+
+                // Поки існує шлях foundFlow від джерела до стоку в залишковій мережі
+                if (foundFlow == 0)
+                    break;
+                maxFlowValue += foundFlow;
+            }
+
+            return maxFlowValue;
+        }
+
+        private int FindPath(int[][] matrix, bool[] visitedVertices, 
+            int vertexFrom, int vertexTo, int flowValue)
+        {
+            if (vertexFrom == vertexTo)
+                return flowValue;
+            visitedVertices[vertexFrom] = true;
+            for (int v = 0; v < visitedVertices.Length; v++)
+                if (!visitedVertices[v] && matrix[vertexFrom][v] > 0)
+                {
+                    // Рекурсивно шукаєм шлях до кожної наступної вершини
+                    int df = FindPath(matrix, visitedVertices, v, vertexTo, 
+                        Math.Min(flowValue, matrix[vertexFrom][v]));
+                    if (df > 0)
+                    {
+                        // Оновлюємо потік відповідно до випадків приналежності до мережі
+                        matrix[vertexFrom][v] -= df;
+                        matrix[v][vertexFrom] += df;
+                        return df;
+                    }
+                }
+            return 0;
+        }
+    }
+}
---
+++ LabsLibrary/Labs/Lab4/Lab4.cs
@@ -0,0 +1,28 @@
+﻿using Common.Core.FileAccess;
+using Common.Core.Interfaces;
+using System;
+
+namespace LabsLibrary.Labs.Lab4
+{
+    class Lab4 : IRunnable
+    {
+        public void Run()
+        {
+            var incidenceMatrix = FileProcessor.GetMatrixFromFile("lab4Data.json", "incidenceMatrix");
+            Console.WriteLine("\nThe incidence matrix is: ");
+            for (int i = 0; i < incidenceMatrix.Length; i++)
+            {
+                for (int j = 0; j < incidenceMatrix[i].Length; j++)
+                {
+                    Console.Write(incidenceMatrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            var ffp = new FordFulkersonProcessor();
+            var result = ffp.CalculateMaxFlow(incidenceMatrix, 0, 2);
+
+            Console.WriteLine($"\nThe max flow value is: {result}");            
+        }
+    }
+}
